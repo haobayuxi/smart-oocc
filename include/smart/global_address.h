efb92f901bbb92a6a712af7e600814b1e32523d7
haobayuxi <whaohit@gmail.com>
2023-11-07 20:29:26 +0800

    update

--- include/smart/global_address.h
+++ include/smart/global_address.h
@@ -1,8 +1,8 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (C) 2022-2023 Feng Ren, Tsinghua University 
- * 
+ * Copyright (C) 2022-2023 Feng Ren, Tsinghua University
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
@@ -25,55 +25,57 @@
 #ifndef SDS_GLOBAL_ADDRESS_H
 #define SDS_GLOBAL_ADDRESS_H
 
+#include <cassert>
 #include <cstdint>
-
 namespace sds {
-    using node_t = uint8_t;
-    using mr_id_t = uint8_t;
+using node_t = uint8_t;
+using mr_id_t = uint8_t;
 
-    const static mr_id_t MAIN_MEMORY_MR_ID = 0;
-    const static mr_id_t DEVICE_MEMORY_MR_ID = 1;
-    const static size_t kMemoryRegions = 2;
+const static mr_id_t MAIN_MEMORY_MR_ID = 0;
+const static mr_id_t DEVICE_MEMORY_MR_ID = 1;
+const static size_t kMemoryRegions = 2;
 
-    union GlobalAddress {
-        struct {
-            uint64_t offset: 48;
-            uint64_t mr_id: 8;
-            uint64_t node: 8;
-        };
-        uint64_t raw;
+union GlobalAddress {
+  struct {
+    uint64_t offset : 48;
+    uint64_t mr_id : 8;
+    uint64_t node : 8;
+  };
+  uint64_t raw;
 
-    public:
-        GlobalAddress(uint64_t raw = UINT64_MAX) noexcept: raw(raw) {}
+ public:
+  GlobalAddress(uint64_t raw = UINT64_MAX) noexcept : raw(raw) {}
 
-        GlobalAddress(node_t node, uint64_t offset) noexcept: node(node), mr_id(MAIN_MEMORY_MR_ID), offset(offset) {}
+  GlobalAddress(node_t node, uint64_t offset) noexcept
+      : node(node), mr_id(MAIN_MEMORY_MR_ID), offset(offset) {}
 
-        GlobalAddress(node_t node, mr_id_t mr_id, uint64_t offset) noexcept: node(node), mr_id(mr_id), offset(offset) {}
+  GlobalAddress(node_t node, mr_id_t mr_id, uint64_t offset) noexcept
+      : node(node), mr_id(mr_id), offset(offset) {}
 
-        GlobalAddress(const GlobalAddress &rhs) noexcept: raw(rhs.raw) {}
+  GlobalAddress(const GlobalAddress &rhs) noexcept : raw(rhs.raw) {}
 
-        GlobalAddress &operator=(const GlobalAddress &rhs) {
-            this->raw = rhs.raw;
-            return *this;
-        }
+  GlobalAddress &operator=(const GlobalAddress &rhs) {
+    this->raw = rhs.raw;
+    return *this;
+  }
 
-        GlobalAddress operator+(uint64_t delta) const {
-            assert(offset + delta >= offset);       // prevent overflow
-            return {raw + delta};
-        }
+  GlobalAddress operator+(uint64_t delta) const {
+    assert(offset + delta >= offset);  // prevent overflow
+    return {raw + delta};
+  }
 
-        bool operator==(const GlobalAddress &rhs) const {
-            return this->raw == rhs.raw;
-        }
+  bool operator==(const GlobalAddress &rhs) const {
+    return this->raw == rhs.raw;
+  }
 
-        bool operator!=(const GlobalAddress &rhs) const {
-            return this->raw != rhs.raw;
-        }
-    };
+  bool operator!=(const GlobalAddress &rhs) const {
+    return this->raw != rhs.raw;
+  }
+};
 
-    static_assert(sizeof(GlobalAddress) == sizeof(uint64_t), "");
+static_assert(sizeof(GlobalAddress) == sizeof(uint64_t), "");
 
-    static const GlobalAddress NULL_GLOBAL_ADDRESS;
-}
+static const GlobalAddress NULL_GLOBAL_ADDRESS;
+}  // namespace sds
 
-#endif //SDS_GLOBAL_ADDRESS_H
+#endif  // SDS_GLOBAL_ADDRESS_H
