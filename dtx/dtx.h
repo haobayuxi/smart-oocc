926a5bbef86daa65f3a14002bfc978edfdfbb468
haobayuxi <whaohit@gmail.com>
2023-11-08 09:37:18 +0800

    update

--- dtx/dtx.h
+++ dtx/dtx.h
@@ -145,8 +145,6 @@ class DTX {
   }
 
   bool TxCommit() {
-    context->EndTask();
-    return true;
     if (is_ro_tx && read_only_set.size() == 1) {
       context->EndTask();
       return true;
