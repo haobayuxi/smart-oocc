c6c5337b256d72fa22b6f3694b00ebf81b1b8802
haobayuxi <whaohit@gmail.com>
2023-11-08 09:40:40 +0800

    update

--- dtx/dtx.h
+++ dtx/dtx.h
@@ -145,10 +145,10 @@ class DTX {
   }
 
   bool TxCommit() {
-    if (is_ro_tx && read_only_set.size() == 1) {
-      context->EndTask();
-      return true;
-    }
+    // if (is_ro_tx && read_only_set.size() == 1) {
+    //   context->EndTask();
+    //   return true;
+    // }
     if (!Validate()) {
       goto ABORT;
     }
