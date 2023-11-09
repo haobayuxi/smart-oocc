0f3f6466d25ae4f7766d23bd45c03a960afc9d7a
haobayuxi <whaohit@gmail.com>
2023-11-09 13:43:32 +0800

    update

--- dtx/dtx.h
+++ dtx/dtx.h
@@ -169,7 +169,7 @@ class DTX {
       for (auto &item : read_only_set) {
         uint64_t read_lease = item.item_ptr.get()->lock;
         if (read_lease < end_time) {
-          //   SDS_INFO("commit lease expired, %ld %ld", read_lease, end_time);
+          SDS_INFO("commit lease expired, %ld %ld", read_lease, end_time);
           goto ABORT;
         }
       }
