eac79ca9a3a25a237278272ff56076099dd0a496
haobayuxi <whaohit@gmail.com>
2023-11-09 12:58:09 +0800

    update

--- dtx/common.h
+++ dtx/common.h
@@ -103,7 +103,7 @@ ALWAYS_INLINE
 uint64_t get_clock_sys_time_us() {
   struct timespec tp;
 
-  clock_gettime(CLOCK_MONOTONIC, &tp);
+  clock_gettime(CLOCK_REALTIME, &tp);
   return (uint64_t)(tp.tv_sec * 1000000 + tp.tv_nsec / 1000);
   //   return (long long)(tp.tv_nsec / 1000);
 }
