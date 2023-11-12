76367e927b55b6f20c8568583ff32644a26a040b
haobayuxi <whaohit@gmail.com>
2023-11-12 15:18:18 +0800

    update

--- dtx/ycsb/bench.cpp
+++ dtx/ycsb/bench.cpp
@@ -47,8 +47,7 @@ bool TxYCSB(tx_id_t tx_id, DTX *dtx) {
   for (int i = 0; i < data_item_size; i++) {
     micro_key_t micro_key;
     if (is_skewed) {
-      // micro_key.item_key = ycsb_client->next();
-      micro_key.item_key = 100;
+      micro_key.item_key = ycsb_client->next();
     } else {
       micro_key.item_key = (itemkey_t)(FastRand(&seed) % (TOTAL_KEYS_NUM - 1));
       // micro_key.item_key = tx_id % (TOTAL_KEYS_NUM - 1);
