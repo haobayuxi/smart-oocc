28503abce2d60c12a013a9a702a2e9fd18d6868c
haobayuxi <whaohit@gmail.com>
2023-11-16 21:15:24 +0800

    update

--- dtx/ycsb/bench.cpp
+++ dtx/ycsb/bench.cpp
@@ -144,9 +144,10 @@ void RunTx(DTXContext *context) {
       timer_idx += threads * coroutines;
       commit_tx++;
       IdleExecution();
-    } else {
-      SDS_INFO("not found?");
     }
+    //  else {
+    //   SDS_INFO("not found?");
+    // }
     // Stat after a million of transactions finish
     if (attempt_tx == ATTEMPTED_NUM) {
       attempts.fetch_add(attempt_tx);
