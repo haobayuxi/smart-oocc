2d51e38a4b7dd191d1184f4ba85914fe2b6bf12f
haobayuxi <whaohit@gmail.com>
2023-11-20 20:44:30 +0800

    update

--- dtx/dtx.cpp
+++ dtx/dtx.cpp
@@ -311,12 +311,12 @@ bool DTX::CheckDirectRO(std::vector<DirectRead> &pending_direct_ro,
         res.item->is_fetched = true;
         // SDS_INFO("lock state %ld, txid = %ld", it->lock, tx_id);
         if (unlikely((it->lock > STATE_READ_LOCKED))) {
-          // if (txn_sys == DTX_SYS::OOCC) {
-          //   re_validate = true;
-          // } else {
-          //   return false;
-          // }
-          return false;
+          if (txn_sys == DTX_SYS::OOCC) {
+            re_validate = true;
+          } else {
+            return false;
+          }
+          // return false;
         }
       } else {
         addr_cache->Insert(res.node_id, it->table_id, it->key, NOT_FOUND);
@@ -364,11 +364,11 @@ bool DTX::CheckHashRO(std::vector<HashRead> &pending_hash_ro,
     if (likely(find)) {
       if (unlikely((it->lock > STATE_READ_LOCKED))) {
         // return false;
-        // if (txn_sys == DTX_SYS::OOCC) {
-        //   re_validate = true;
-        // } else {
-        return false;
-        // }
+        if (txn_sys == DTX_SYS::OOCC) {
+          re_validate = true;
+        } else {
+          return false;
+        }
       }
     } else {
       return false;
@@ -469,11 +469,11 @@ bool DTX::CheckNextHashRO(std::list<HashRead> &pending_next_hash_ro) {
         // context->read(cas_buf, GlobalAddress(res.node_id, lock_offset),
         //               sizeof(lock_t));
         // return false;
-        // if (txn_sys == DTX_SYS::OOCC) {
-        //   re_validate = true;
-        // } else {
-        return false;
-        // }
+        if (txn_sys == DTX_SYS::OOCC) {
+          re_validate = true;
+        } else {
+          return false;
+        }
       }
       iter = pending_next_hash_ro.erase(iter);
     } else {
