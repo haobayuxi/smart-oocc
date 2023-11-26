d129e26ad82fe2e0a55fa45bb5df23d250f7c511
haobayuxi <whaohit@gmail.com>
2023-11-26 09:20:19 +0800

    update

--- dtx/dslr.cpp
+++ dtx/dslr.cpp
@@ -657,7 +657,7 @@ bool DTX::DSLRCheckNextHashRW(std::list<CasRead> &pending_next_cas_rw,
                     DataItemSize);
       context->PostRequest();
     } else {
-      return false;
+      // return false;
       auto *local_hash_node = (HashNode *)res.buf;
       if (local_hash_node->next == nullptr) return false;
       auto node_off = (uint64_t)local_hash_node->next - res.meta.data_ptr +
@@ -871,8 +871,8 @@ bool DTX::DSLRCommit() {
   context->Sync();
 
   while (reset.size() > 0) {
-    CheckReset();
     context->Sync();
+    CheckReset();
   }
   return true;
 }
@@ -946,8 +946,8 @@ bool DTX::DSLRAbort() {
   }
   context->Sync();
   while (reset.size() > 0) {
-    CheckReset();
     context->Sync();
+    CheckReset();
   }
   return true;
 }
