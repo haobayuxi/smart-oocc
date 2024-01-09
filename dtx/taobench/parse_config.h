2decd2f39f7ee216e2a549bc79ef15298eaa9698
haobayuxi <whaohit@gmail.com>
2024-01-09 15:40:02 +0800

    update

--- dtx/taobench/parse_config.h
+++ dtx/taobench/parse_config.h
@@ -1,45 +1,44 @@
 #pragma once
 
+#include <algorithm>
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <regex>
+#include <sstream>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
-#include <fstream>
-#include <algorithm>
-#include <regex>
 #include <vector>
-#include <random>
-#include <iostream>
-#include <sstream>
-#include <cassert>
 
 namespace benchmark {
-  class ConfigParser {
+class ConfigParser {
+ public:
+  ConfigParser(std::string configFilepath);
+  void printOut() const;  // for testing
 
-    public:
+  struct LineObject {
+    LineObject(std::string const& line);
 
-      ConfigParser(std::string const & configFilepath);
-      void printOut() const; // for testing
+    LineObject() {
+      throw std::invalid_argument(
+          "Attempted to construct a line object that does not exist!");
+    }
 
-      struct LineObject {
-        LineObject(std::string const & line);
+    template <class T>
+    std::vector<T> parseList(std::string const& list,
+                             const std::function<T(std::string&)>& f);
 
-        LineObject() {
-          throw std::invalid_argument("Attempted to construct a line object that does not exist!");
-        }
+    void printOut() const;  // for testing
 
-        template <class T>
-        std::vector<T> parseList(std::string const & list, 
-                                 const std::function<T(std::string&)>& f);
-
-        void printOut() const; // for testing
-
-        std::string name;
-        std::vector<std::string> types;
-        std::vector<int> vals;
-        std::vector<double> weights;
-        std::discrete_distribution<> distribution;
-      };
-
-      std::unordered_map<std::string, LineObject> fields;
+    std::string name;
+    std::vector<std::string> types;
+    std::vector<int> vals;
+    std::vector<double> weights;
+    std::discrete_distribution<> distribution;
   };
-}
+
+  std::unordered_map<std::string, LineObject> fields;
+};
+}  // namespace benchmark
