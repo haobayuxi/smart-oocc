749e2e10bc7e2c20f33e8558866ac60679e0d20c
haobayuxi <whaohit@gmail.com>
2024-01-10 19:07:53 +0800

    update

--- dtx/taobench/tao.h
+++ dtx/taobench/tao.h
@@ -84,8 +84,8 @@ class TAO {
   TAO() {
     config_parser = ConfigParser();
     edge_count = 0;
-    // LoadEdges();
-    cout << "load edges done" << endl;
+    LoadEdges();
+    cout << "load edges done=" << edge_count << endl;
   }
 
   void LoadEdges() {
@@ -97,13 +97,13 @@ class TAO {
       file >> remote;
       uint64_t shard = primary >> 57;
       if (shard > 50) {
-        cout << "read shard=" << shard << endl;
-        assert(0);
+        continue;
       }
       shard_to_edges[shard].push_back(Edge{
           primary,
           remote,
       });
+      edge_count++;
     }
     file.close();
   }
@@ -146,18 +146,18 @@ class TAO {
     ConfigParser::LineObject &remote_shards =
         config_parser.fields["remote_shards"];
     uint8_t value[150] = {'a'};
-    ofstream file("tao.dat");
-    // ifstream file("tao.dat");
+    // ofstream file("tao.dat");
+    ifstream file("tao.dat");
     for (int i = 0; i < TOTAL_EDGES_NUM; i++) {
       int primary_shard = unif(gen);
       int remote_shard = remote_shards.distribution(gen);
-      uint64_t primary_key = GenerateKey(primary_shard);
-      uint64_t remote_key = GenerateKey(remote_shard);
+      // uint64_t primary_key = GenerateKey(primary_shard);
+      // uint64_t remote_key = GenerateKey(remote_shard);
 
-      // uint64_t primary_key = 0;
-      // uint64_t remote_key = 0;
-      // file >> primary_key;
-      // file >> remote_key;
+      uint64_t primary_key = 0;
+      uint64_t remote_key = 0;
+      file >> primary_key;
+      file >> remote_key;
       Edge e = Edge{
           primary_key,
           remote_key,
@@ -186,8 +186,8 @@ class TAO {
           edge_key, item_to_be_inserted3, mem_store_reserve_param);
       inserted_item3->remote_offset =
           edge_table->GetItemRemoteOffset(inserted_item3);
-      file << primary_key;
-      file << remote_key;
+      // file << primary_key;
+      // file << remote_key;
     }
     file.close();
   }
