1a1c42912463581e3f84177e7a04ad3bb28e2231
haobayuxi <whaohit@gmail.com>
2024-01-10 14:26:55 +0800

    update

--- dtx/taobench/tao.h
+++ dtx/taobench/tao.h
@@ -119,7 +119,8 @@ class TAO {
 
   Edge const &GetRandomEdge() {
     ConfigParser::LineObject &obj = config_parser.fields["primary_shards"];
-    auto it = shard_to_edges[gen];  // 从 primary shard 拿到shard的key
+    auto it = shard_to_edges[obj.distribution(
+        gen)];  // 从 primary shard 拿到shard的key
 
     std::uniform_int_distribution<int> edge_selector(0, it.size() - 1);
     return it[edge_selector(gen)];
