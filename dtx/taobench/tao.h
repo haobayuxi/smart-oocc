1e4f204b00bc92f39c0c0cbb4d44db4fb528156a
haobayuxi <whaohit@gmail.com>
2024-01-11 16:17:56 +0800

    update

--- dtx/taobench/tao.h
+++ dtx/taobench/tao.h
@@ -325,7 +325,7 @@ class TAO {
       // random a edge
       // random read edge or object
       int op = op_obj.distribution(gen);
-      op = 0;
+      op = 1;
       int primary_shard = primary_shards.distribution(gen);
       int remote_shard = remote_shards.distribution(gen);
       uint64_t primary_key = GenerateKey(primary_shard);
