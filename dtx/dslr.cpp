06ae9fdd79a7738b29bf1be2e98f7925c5314fb7
haobayuxi <whaohit@gmail.com>
2024-05-08 13:07:40 +0800

    update

--- dtx/dslr.cpp
+++ dtx/dslr.cpp
@@ -753,11 +753,12 @@ bool DTX::DSLRCheckCasRW(std::vector<CasRead> &pending_cas_rw,
                                 .version = fetched_item->version});
       } else {
         if (likely(fetched_item->valid)) {
-                      result =false;
-          }
-          //  {
+                    //  {
           //   result = false;
           // }
+          if (fetched_item->remote_offset != it->remote_offset) {
+                      result =false;
+          }
           *it = *fetched_item;
           re.item->is_fetched = true;
           uint64_t lock = *(lock_t *)re.cas_buf;
