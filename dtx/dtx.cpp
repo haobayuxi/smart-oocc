8c37d2e39c3a399a950b7790096f433e7ef16cf0
haobayuxi <whaohit@gmail.com>
2024-09-26 15:25:10 +0800

    update

--- dtx/dtx.cpp
+++ dtx/dtx.cpp
@@ -4,8 +4,8 @@
 #include "dtx.h"
 
 bool CheckReadWriteConflict = true;
-bool DelayLock = true;
-bool WriteUnlock = false;
+bool DelayLock = false;   // set to true to open
+bool WriteUnlock = true;  // set to false to open
 
 DTX::DTX(DTXContext *context, int _txn_sys, int _lease, bool _delayed,
          double _offset)
